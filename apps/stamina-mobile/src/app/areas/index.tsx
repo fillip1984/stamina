24b56cf04f952291f6a5a63fdbae8186651a73ed
fillip1984 <fillip1984@gmail.com>
2025-11-23 10:49:35 -0500

    stubbed out results and areas pages for readonly

--- apps/stamina-mobile/src/app/areas/index.tsx
+++ apps/stamina-mobile/src/app/areas/index.tsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { View } from "react-native";
+import { useQuery } from "@tanstack/react-query";
 
 import Container from "~/components/ui/container";
 import Typography from "~/components/ui/typography";
+import { trpc } from "~/utils/api";
+import { AreaType } from "../../../../../packages/api/dist/types";
 
 export default function AreasPage() {
+  const areas = useQuery(trpc.area.findAll.queryOptions());
   return (
     <Container>
-      <Typography variant="heading">Areas Page</Typography>
+      <View className="flex gap-2">
+        {areas.data?.map((area) => (
+          <AreaCard key={area.id} area={area} />
+        ))}
+      </View>
     </Container>
   );
 }
+
+const AreaCard = ({ area }: { area: AreaType }) => {
+  return (
+    <View className="rounded-lg border border-white p-2">
+      <Typography variant={"heading"}>{area.name}</Typography>
+      <Typography variant={"muted"}>{area.description}</Typography>
+    </View>
+  );
+};
--- apps/stamina-mobile/src/app/results/index.tsx
+++ apps/stamina-mobile/src/app/results/index.tsx
@@ -1,12 +1,57 @@
 import React from "react";
+import { View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { useQuery } from "@tanstack/react-query";
+import { format } from "date-fns";
+
+import { ResultType } from "@stamina/api";
 
 import Container from "~/components/ui/container";
 import Typography from "~/components/ui/typography";
+import { trpc } from "~/utils/api";
 
 export default function ResultsPage() {
+  const results = useQuery(trpc.result.findAll.queryOptions());
   return (
     <Container>
-      <Typography variant="heading">Results Page</Typography>
+      <View className="flex gap-2">
+        {results.data?.map((result) => (
+          <ResultCard key={result.id} result={result} />
+        ))}
+      </View>
     </Container>
   );
 }
+
+const ResultCard = ({ result }: { result: ResultType }) => {
+  if (result.bloodPressureReading) {
+    return (
+      <View className="rounded-lg border border-white p-2">
+        <Typography variant={"heading"}>
+          Blood Pressure: {result.bloodPressureReading.systolic}/
+          {result.bloodPressureReading.diastolic} mmHg
+        </Typography>
+        <Typography variant={"muted"}>{format(result.date, "PPP")}</Typography>
+      </View>
+    );
+  }
+
+  if (result.weighIn) {
+    return (
+      <View className="flex-row rounded-lg border border-white p-2">
+        <Ionicons name="scale-outline" size={24} color="white" />
+        <Typography variant={"heading"}>
+          Weight: {result.weighIn.weight} lbs
+        </Typography>
+        <Typography variant={"muted"}>{format(result.date, "PPP")}</Typography>
+      </View>
+    );
+  }
+
+  return (
+    <View className="rounded-lg border border-white p-2">
+      <Typography variant={"heading"}>{result.notes}</Typography>
+      <Typography variant={"muted"}>{format(result.date, "PPP")}</Typography>
+    </View>
+  );
+};
